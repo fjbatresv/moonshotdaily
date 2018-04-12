5e4d7014c74b68c46b8b8044981ee89826a9ebfc
fjbatresv <fjbatresv@gmail.com>
2018-04-12 16:18:17 -0600

    Function for new user and moon shot generator

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,43 @@
 */
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-exports.newUser = functions.auth.user().onCreate(event => {
-    const user = event.data;
-    admin.database().ref('users/' + user.uid ).set(user);
+admin.initializeApp();
+
+exports.newUser = functions.auth.user().onCreate((userMetadata, context) => {
+    console.log('user', userMetadata);
+    return admin.database().ref('user/' + userMetadata.uid).set({
+        uid: userMetadata.uid
+    });
 });
+
+exports.createShot = functions.https.onRequest((req, res) => {
+   return admin.database().ref('problems').once('value').then((datasnapshot) => {
+        var problems = [];
+        datasnapshot.forEach((child) => {
+            problems.push(child.key);
+        });
+        return admin.database().ref('tech').once('value').then((snapshot) => {
+            var techs = [];
+            snapshot.forEach((child) => {
+                techs.push(child.key);
+            });
+            var date = new Date();
+            date.setHours(0, 0, 0, 0);
+            return admin.database().ref('moonshot/').push({
+                tech: techs.randomElement(),
+                problem: problems.randomElement(),
+                time: date.getTime()
+            });
+        }).catch((ex) => {
+            console.log(ex);
+            return;
+        });
+    }).catch((ex) => {
+        console.log(ex);
+        return;
+    });
+});
+
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
